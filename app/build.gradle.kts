70151ca8628e1c5907c47297ff501935e6ba5783
muffler88 <muffler88@gmail.com>
2026-08-30 22:59:06 +0300

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.dateswitcher"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.example.dateswitcher"
+        minSdk = 26
+        targetSdk = 37
+        versionCode = 2
+        versionName = "2.0"
+    }
+}
